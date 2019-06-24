3088add936724a9d558ab4213f5553765e1ac60d
HiAlfiiaHiAlfiia@github.com <HiAlfiia@github.com>
2019-06-24 21:25:54 +0400

    ЛР1 (task-1705)

---
+++ dist/src/cart.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  update();
+};
+
+var update = function(){
+  updateBadge();
+  var cou =0;
+  var fullSum = 0;
+  var table = document.getElementById("tbody");
+  table.innerHTML="";
+  for (var i = 0; i < localStorage.length; i++){
+    var ind = localStorage.key(i);
+    if(localStorage[ind]<1){continue;}
+    cou+=1;
+    var product = document.createElement('tr');
+    product.id=ind;
+    var score = document.createElement('th');
+    score.innerHTML=cou;
+    product.appendChild(score);
+    var city = document.createElement('td');
+    city.innerHTML=allProducts[ind].name;
+    product.appendChild(city);
+    var price = document.createElement('td');
+    price.innerHTML=allProducts[ind].price+" руб";
+    product.appendChild(price);
+    var count = document.createElement('td');
+    var countInp = document.createElement('input');
+    countInp.type = "number"
+    countInp.className="form-control cart-number";
+    countInp.value=localStorage[ind];
+    countInp.min="1"
+    countInp.onchange=function(event){
+      var id = +event.target.parentElement.parentElement.id;
+      localStorage[id]=+event.target.value;
+      update();
+    }
+    count.appendChild(countInp);
+    product.appendChild(count);
+    var sum = document.createElement('td');
+    sum.innerHTML=Number(localStorage[ind])*allProducts[ind].price + ' руб';
+    fullSum+=Number(localStorage[ind])*allProducts[ind].price;
+    product.appendChild(sum);
+    var del = document.createElement('td');
+    var delB = document.createElement('button');
+    delB.type = "button"
+    delB.className="btn btn-danger";
+    delB.innerHTML="Удалить";
+    delB.onclick = function(event){
+      var id = +event.target.id;
+      localStorage[id]=0;
+      update();
+    }
+    del.appendChild(delB);
+    product.appendChild(del);
+    table.appendChild(product);
+  }
+  var res = document.createElement('tr');
+  res.id="res";
+  var empty = document.createElement('th');
+  empty.scope="row";
+  empty.colSpan="3";
+  empty.innerHTML="&nbsp;";
+  var fSumText = document.createElement('td');
+  fSumText.innerHTML="Итого: ".bold();
+  var fSum = document.createElement('td');
+  fSum.innerHTML=fullSum+' руб';
+  res.appendChild(empty);
+  res.appendChild(fSumText);
+  res.appendChild(fSum);
+  table.appendChild(res);
+}
---
+++ dist/src/st.js
@@ -0,0 +1,27 @@
+﻿class Product {
+  constructor(name, price, country, img) {
+    this.name = name;
+		this.price = price;
+		this.country = country;
+    this.img = img;
+  }
+}
+
+var countries={"1":"Россия", "2":"Франция", "3":"Таиланд"}
+
+var allProducts = [
+    new Product('Москва', 150000, 'Россия', '0'),
+    new Product('Сочи', 200000, 'Россия', '1'),
+    new Product('Париж', 400000, 'Франция', '2'),
+    new Product('Бангкок', 50000, 'Таиланд', '3'),
+    new Product('Пухкет', 60000, 'Таиланд', '4')
+];
+
+var updateBadge = function(){
+  var count = 0;
+  for (var i = 0; i < localStorage.length; i++){
+     count+= Number(localStorage.getItem(localStorage.key(i)));
+   }
+  var bc = document.getElementById("badgeCount");
+  bc.innerHTML = count;
+}
